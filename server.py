4cddc1ce9474b27ed912461fab32a39aedf547ae
Ruslan Balkin <baron@bytexgames.ru>
2019-02-26 17:57:10 +0300

    Improved code

--- server.py
+++ server.py
@@ -27,9 +27,18 @@ query_ports = "SELECT mac_address, device_id from neutron.ports"
 # TODO: Refactor it
 ports_dict = {}
 ports_cursor = conn.cursor()
-ports_cursor.execute(query_ports)
-for (mac_address, device_id) in ports_cursor:
-    ports_dict[device_id] = mac_address
+
+try:
+    ports_cursor.execute(query_ports)
+
+    for (mac_address, device_id) in ports_cursor:
+        if device_id in ports_dict:
+            ports_dict[device_id].append(mac_address)
+        else:
+            ports_dict[device_id] = [mac_address]
+
+finally:
+    ports_cursor.close()
 
 class OpenstackHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -42,13 +51,16 @@ class OpenstackHandler(BaseHTTPRequestHandler):
         response = []
 
         instances_cursor = conn.cursor()
-        instances_cursor.execute(query_instances)
-        for x in instances_cursor.fetchall():
-            # Пишу как явист без list comprehensions, но потом отрефакторю по-питоньи
-            entry = {"instance_id": x[0], "interfaces": [ports_dict[x[0]]]}
-            response.append(entry)
+        try:
+            instances_cursor.execute(query_instances)
+            for (uuid, display_name) in instances_cursor.fetchall():
+                # Пишу как явист без list comprehensions, но потом отрефакторю по-питоньи
+                entry = {"instance_id": uuid, "interfaces": ports_dict[uuid]}
+                response.append(entry)
+        finally:
+            instances_cursor.close()
 
-        self.wfile.write(bytes(json.dumps(response), 'utf8'))
+        self.wfile.write(bytes(json.dumps(response, indent=True), 'utf8'))
 
 
 # with почему-то не работает =(
