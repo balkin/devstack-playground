18af77af2b4055d68c21fc92681a61344262dce7
Ruslan Balkin <baron@baron.su>
2019-02-27 03:07:08 +0300

    Big refactoring.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-PREVED
+# Openstack info
+
+* curl -v http://localhost:8000/realtime
+* curl -v http://localhost:8000/cached
+* curl -v http://localhost:8000/joined
--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 import json
 import socketserver
+import time
 from http.server import BaseHTTPRequestHandler
+
 import mysql.connector
+
+INDEX_HTML = """<html><body>
+            <a href="/realtime">first</a> | <a href="/cached">second</a> | <a href="/joined">third</a>
+            </body></html>"""
 
 PORT = 8000
 
-# Handler = http.server.SimpleHTTPRequestHandler
 # Запустить пару виртуальных машин (можно 2)
 # Из базы достать: 
 # SELECT uuid, display_name from instances where vm_state = 'active'
@@ -18,59 +23,118 @@ PORT = 8000
 # ]
 # }
 # ]
-#
 
 conn = mysql.connector.connect(user='root', password='Passw0rd', database='nova_cell1')
 query_instances = "SELECT uuid, display_name from instances where vm_state = 'active'"
 query_ports = "SELECT mac_address, device_id from neutron.ports"
+query_one_port = "SELECT mac_address, device_id from neutron.ports WHERE device_id = %(uuid)s"
+query_combined = "SELECT uuid, display_name, mac_address FROM instances JOIN neutron.ports ON device_id = uuid WHERE vm_state = 'active'"
 
-# TODO: Refactor it
+# Global variables used for cache
 ports_dict = {}
-ports_cursor = conn.cursor()
+ports_ts = 0
 
-try:
-    ports_cursor.execute(query_ports)
-
-    for (mac_address, device_id) in ports_cursor:
-        if device_id in ports_dict:
-            ports_dict[device_id].append(mac_address)
-        else:
-            ports_dict[device_id] = [mac_address]
-
-finally:
-    ports_cursor.close()
 
 class OpenstackHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Send response status code
+    def __init__(self, request, client_address, server):
+        super().__init__(request, client_address, server)
+
+    def ok_headers(self, content_type='application/json'):
         self.send_response(200)
-
         # Send headers
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', content_type)
         self.end_headers()
-        response = []
 
+    # This method is used for list comprehensions in /realtime
+    def one_instance(self, ports_cursor, uuid):
+        ports_cursor.execute(query_one_port, {"uuid": uuid})
+        return {"instance_id": uuid, "interfaces": [mac for (mac, device_id) in ports_cursor.fetchall()]}
+
+    def realtime(self):
+        instances_cursor, ports_cursor = conn.cursor(), conn.cursor()
+        try:
+            instances_cursor.execute(query_instances)
+            response = [self.one_instance(ports_cursor, uuid) for (uuid, display_name) in instances_cursor.fetchall()]
+            self.wfile.write(bytes(json.dumps(response, indent=True), 'utf8'))
+        finally:
+            instances_cursor.close()
+            ports_cursor.close()
+
+    # This method is used for updating ports data in /cached
+    def maybe_update_cached_ports(self):
+        global ports_ts, ports_dict
+        if time.time() > ports_ts + 10:
+            ports_ts = time.time()
+            ports_cursor = conn.cursor()
+            try:
+                ports_cursor.execute(query_ports)
+                ports_dict.clear()
+
+                for (mac_address, device_id) in ports_cursor.fetchall():
+                    if device_id in ports_dict:
+                        ports_dict[device_id].append(mac_address)
+                    else:
+                        ports_dict[device_id] = [mac_address]
+
+            finally:
+                ports_cursor.close()
+
+    def cached(self):
+        self.maybe_update_cached_ports()
         instances_cursor = conn.cursor()
         try:
             instances_cursor.execute(query_instances)
-            for (uuid, display_name) in instances_cursor.fetchall():
-                # Пишу как явист без list comprehensions, но потом отрефакторю по-питоньи
-                entry = {"instance_id": uuid, "interfaces": ports_dict[uuid]}
-                response.append(entry)
+            response = [{"instance_id": uuid, "interfaces": ports_dict[uuid]} for (uuid, display_name) in
+                        instances_cursor.fetchall()]
+            self.wfile.write(bytes(json.dumps(response, indent=True), 'utf8'))
         finally:
             instances_cursor.close()
 
-        self.wfile.write(bytes(json.dumps(response, indent=True), 'utf8'))
+    def joined(self):
+        combined_cursor = conn.cursor()
+        try:
+            combined_cursor.execute(query_combined)
+            response_dict = {}
+            for (uuid, display_name, mac_address) in combined_cursor.fetchall():
+                if uuid in response_dict:
+                    response_dict[uuid].append(mac_address)
+                else:
+                    response_dict[uuid] = [mac_address]
+
+            response = [{"instance_id": uuid, "interfaces": response_dict[uuid]} for uuid in response_dict]
+            self.wfile.write(bytes(json.dumps(response, indent=True), 'utf8'))
+        finally:
+            combined_cursor.close()
+
+    def do_GET(self):
+        # Send response status code
+        if self.path == '/':
+            self.ok_headers("text/html")
+            self.wfile.write(bytes(INDEX_HTML, "utf8"))
+        elif self.path == '/realtime':
+            self.ok_headers()
+            self.realtime()
+        elif self.path == '/cached':
+            self.ok_headers()
+            self.cached()
+        elif self.path == '/joined':
+            self.ok_headers()
+            self.joined()
+        else:
+            self.send_response_only(404, "Not found")
 
 
-# with почему-то не работает =(
+socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer(("", PORT), OpenstackHandler)
 print("Listening on {}".format(PORT))
 try:
     httpd.serve_forever()
 except KeyboardInterrupt:
     print("Server stopped on keyboard interrupt")
+except:
+    print("Server stopped on exception")
 finally:
     httpd.server_close()
+    conn.close()
 
 print("DONE")
